3b516e8be67907b94875ce44311c31f2c2752b86
Shashi Raja <sraja@Ban-1Sraja-m.local>
2018-10-29 14:27:48 +0530

    added new tests

--- src/tests/test_with_parameters.py
+++ src/tests/test_with_parameters.py
@@ -1,5 +1,10 @@
 import pytest
+from ..sum import *
 
 @pytest.mark.parametrize('num1, num2, expected', [(3,5,8)])
-def test_sum(num1, num2, expected):
+def test_sum_with_one_value(num1, num2, expected):
+        assert sum(num1, num2) == expected
+
+@pytest.mark.parametrize('num1, num2, expected',[(3,5,8),(-2,-2,-4), (-1,5,4), (3,-5,-2), (0,5,5)])
+def test_sum_with_multi_value(num1, num2, expected):
         assert sum(num1, num2) == expected
